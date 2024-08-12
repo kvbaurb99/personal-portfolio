69b98e8cbe72b932b82473330bb32fda0bf98985
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 22:40:29 +0200

    styles + animations

--- src/styles/main.ts
+++ src/styles/main.ts
@@ -1,3 +1,29 @@
-// every page has own style files in their folder
-"use client"
-import styled from "styled-components"
+"use client";
+import styled, { css } from "styled-components";
+
+type SectionProps = {
+  $type?: "about" | "skills";
+};
+
+export const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Section = styled.section<SectionProps>`
+  color: #000;
+  width: 65%;
+  margin: 0 auto;
+  margin-top: 3rem;
+  ${(props) =>
+    props.$type === "about" &&
+    css`
+      display: flex;
+
+      gap: 2rem;
+    `}
+`;
