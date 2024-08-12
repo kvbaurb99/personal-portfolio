e7aec12906053cea59d7d13696e04c16c4d7adbc
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 22:41:05 +0200

    create data for components

---
+++ src/data/meta/meta.ts
@@ -0,0 +1,18 @@
+export const keywords =
+  "portfolio, jakub, urbański, bielsko-biała, bielsko, biała, web, developer, web developer, programista, programista web, programista webowy, webowy, webowa, strona, strona internetowa, strona www, internetowa, www, html, css, javascript, react, next, next.js, gatsby, gatsby.js, strapi, strapi.io, cms, headless, headless cms, headless-cms";
+
+export const openGraph = {
+  title: "Bielsko-Biała | Frontend Developer - Jakub Urbański",
+  description: "Jakub Urbański - Frontend Developer Portfolio",
+  url: `https://jakuburbanski.pl`,
+  siteName: "Jakub Urbański",
+  images: [
+    {
+      url: "",
+      width: 800,
+      height: 600,
+    },
+  ],
+  locale: "pl",
+  type: "website",
+};
---
+++ src/data/sections/about/data.ts
@@ -0,0 +1,6 @@
+export const aboutData = {
+  title:
+    "Nazywam się Jakub Urbański, jestem doświadczonym Front-end Developer'em pochodzącym z Bielska Białej.",
+  overview:
+    "Specjalizuję się w tworzeniu funkcjonalnych i responsywnych stron internetowych, które są zarówno estetyczne, jak i intuicyjne w obsłudze. Dzięki znajomosci technologii takich jak HTML, CSS, JavaScript, React, oraz Next.js jestem w stanie stworzyć stronę internetową od zera, aż po jej wdrożenie. Utworzona strona jest zoptymalizowana pod kątem SEO oraz wydajności co pozwala na jej dobre pozycjonowanie w wynikach wyszukiwania. Dodatkowo posiadam umiejętności korzystania ze sztucznej inteligencji i jej implementacji w projektach webowych.",
+};
