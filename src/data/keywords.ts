348538187a7a6f61ef1e9f8a91745d73de46f2bd
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 23:43:13 +0200

    update keywords

--- src/data/keywords.ts
+++
@@ -1,2 +0,0 @@
-export const keywords =
-  "portfolio, jakub, urbański, bielsko-biała, bielsko, biała, web, developer, web developer, programista, programista web, programista webowy, webowy, webowa, strona, strona internetowa, strona www, internetowa, www, html, css, javascript, react, next, next.js, gatsby, gatsby.js, strapi, strapi.io, cms, headless, headless cms, headless-cms";
