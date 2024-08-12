ead1a82b0d1cda91fa21eef3b853b99910130f02
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 22:41:24 +0200

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,37 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "@/lib/registry";
-import { keywords } from "@/data/keywords";
-import { Inter } from "next/font/google";
+import { keywords, openGraph } from "@/data/meta/meta";
+import { Poppins, Lato, Kaushan_Script } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: "Jakub Urbański - Portfolio",
-  description: "Strona portfolio Jakuba Urbańskiego",
+  title: "Bielsko-Biała | Frontend Developer - Jakub Urbański",
+  description: "Jakub Urbański - Frontend Developer Portfolio",
   keywords,
   robots: {
     index: true,
     follow: true,
   },
+  openGraph,
 };
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+});
+
+const latto = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-lato",
+});
+
+const kaushan = Kaushan_Script({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-kaushan",
+});
 
 export default function RootLayout({
   children,
@@ -22,9 +39,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    <html lang="pl">
+      <body
+        className={`${poppins.variable} ${latto.variable} ${kaushan.variable}`}
+      >
+        <main>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </main>
       </body>
     </html>
   );
