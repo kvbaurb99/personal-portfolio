f3009039b4c212af11f3d4a5d289f40881921196
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 22:40:44 +0200

    implement components into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Navigation from "@/components/navigation";
+import AboutMe from "@/components/sections/about";
+import SkillsSection from "@/components/sections/skills";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <AboutMe />
+      <SkillsSection />
+      <Footer />
+    </>
+  );
 }
