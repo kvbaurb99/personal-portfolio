15c67175256f39033e6120bc05a8bf022509d1c7
kvbaurb_99 <kvbaurbanski2@gmail.com>
2024-08-12 22:39:13 +0200

    create navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,5 +1,21 @@
+import { Nav, NavContainer, NavItem, NavList } from "./styles/navigation";
+import Logo from "@/assets/logo/J.png";
+import items from "@/data/navigation/items.json";
+import Image from "next/image";
+
 export default function Navigation() {
   return (
-    <nav></nav>
-  )
+    <Nav>
+      <NavContainer>
+        <Image src={Logo} width={40} height={40} priority alt="logo" />
+        <NavList>
+          {items.map((item, index) => (
+            <NavItem key={index}>
+              <a href="#about">{item.title}</a>
+            </NavItem>
+          ))}
+        </NavList>
+      </NavContainer>
+    </Nav>
+  );
 }
---
+++ src/components/navigation/styles/navigation.ts
@@ -0,0 +1,25 @@
+"use client";
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  position: fixed;
+  width: 100%;
+  z-index: 100;
+  top: 0;
+  left: 0;
+`;
+
+export const NavContainer = styled.nav`
+  width: 65%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem 0;
+`;
+export const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+  color: rgba(255, 255, 255, 0.8);
+  gap: 2rem;
+`;
+export const NavItem = styled.li``;
