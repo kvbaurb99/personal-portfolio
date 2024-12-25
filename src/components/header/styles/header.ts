3d386b3c83b426cc2d67c95001e8a27c783f779e
kvbaurb99 <kvbaurbanski2@gmail.com>
2024-12-25 23:10:39 +0100

    update header

--- src/components/header/styles/header.ts
+++ src/components/header/styles/header.ts
@@ -9,9 +9,6 @@ export const HeaderContainer = styled.header`
   align-items: center;
   height: 100vh;
   width: 100%;
-  @media (max-width: 768px) {
-    height: 615px;
-  }
 `;
 
 export const HeaderTitle = styled.p`
