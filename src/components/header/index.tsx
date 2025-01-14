6e4d95d29153b7268f50af17bfc265a25cda3673
kvbaurb99 <kvbaurbanski2@gmail.com>
2025-01-14 18:44:18 +0100

    Add mobile responsiveness to Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,21 +8,27 @@ import { Backdrop } from "@/styles/main";
 import Content from "./elements/Content";
 import ArrowDown from "./elements/ArrowDown";
 
+type Props = {
+  isMobile: boolean;
+};
+
 const MotionImage = motion(Image);
 
 const AnimatedBackgroundImage = styled(MotionImage)`
   will-change: transform;
 `;
 
-export default function Header() {
+export default function Header({ isMobile }: Props) {
   return (
     <HeaderContainer>
       <AnimatedBackgroundImage
         src={Background}
-        fill
+        width={isMobile ? 780 : 1440}
+        quality={100}
+        height={560}
         priority
         alt="Header background"
-        className="header-fade object-cover lg:object-fill"
+        className="header-fade object-cover lg:object-fill absolute top-0 left-0 w-full h-full"
         initial={{ scale: 1.25 }}
         animate={{ scale: 1 }}
         transition={{
